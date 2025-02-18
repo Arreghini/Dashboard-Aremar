5839d49bbcb0c084e53e720652f5c7d2563e652e
Mauricio Arreghini <marreghi@gmail.com>
2025-02-18 00:36:55 -0300

    integración de mercadopago

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -13,4 +12,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
